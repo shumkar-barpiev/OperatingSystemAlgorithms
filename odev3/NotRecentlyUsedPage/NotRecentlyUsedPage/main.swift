865200629b7c2cea41d5748e719597e74f54ab96
Shumkar-Barpiev <barpievsh@gmail.com>
2022-12-12 21:55:36 +0600

    implementing homework by OOP

---
+++ odev3/NotRecentlyUsedPage/NotRecentlyUsedPage/main.swift
@@ -0,0 +1,360 @@
+//
+//  main.swift
+//  NotRecentlyUsedPage
+//
+//  Created by User on 12/12/22.
+//
+
+import Foundation
+
+class Page{
+    var pageId = 0
+    var pageNumber = 0
+}
+
+class PageTable: Page{
+    var pageLocation = 0
+    var referenced = 0
+    var modified = 0
+}
+
+func completePageTable(_ ram: [Page], _ hdd: [Page]) -> [PageTable]{
+    var pageTable = [PageTable]()
+    
+    if ram.count > 0{
+        for page in ram{
+            let pageTableObj = PageTable()
+            pageTableObj.pageId = page.pageId
+            pageTableObj.pageNumber = page.pageNumber
+            pageTableObj.pageLocation = 1
+            pageTableObj.referenced = 0
+            pageTableObj.modified = 0
+            
+            pageTable.append(pageTableObj)
+        }
+    }
+    
+    if hdd.count > 0{
+        for page in hdd{
+            let pageTableObj = PageTable()
+            pageTableObj.pageId = page.pageId
+            pageTableObj.pageNumber = page.pageNumber
+            pageTableObj.pageLocation = 0
+            pageTableObj.referenced = 0
+            pageTableObj.modified = 0
+            
+            pageTable.append(pageTableObj)
+        }
+    }
+    return pageTable
+}
+
+func deletePage(_ pageId: Int){
+    for page in pageTables{
+        if page.pageId == pageId{
+            if page.pageLocation == 1{
+                var pageIndex = 0
+                
+                for i in 0...RAM.count-1{
+                    if RAM[i].pageId == page.pageId && RAM[i].pageNumber == page.pageNumber{
+                        pageIndex = i
+                        break
+                    }
+                }
+                RAM.remove(at: pageIndex)
+            }else if page.pageLocation == 0{
+                var pageIndex = 0
+                for i in 0...HDD.count-1{
+                    if HDD[i].pageId == page.pageId && HDD[i].pageNumber == page.pageNumber{
+                        pageIndex = i
+                        break
+                    }
+                }
+                HDD.remove(at: pageIndex)
+            }
+        }
+    }
+}
+
+func notRecentlyUsedPage(_ ram: [Page]) -> [Page]{
+    var tempRAM = ram
+    var notReferencedNotModified = [Page]()
+    var notReferencedModified = [Page]()
+    var referencedNotModified = [Page]()
+    var referencedModified = [Page]()
+    
+    
+    for ramPage in ram{
+        for page in pageTables{
+            if ramPage.pageId == page.pageId{
+                if page.referenced == 0 && page.modified == 0{
+                    notReferencedNotModified.append(ramPage)
+                } else if page.referenced == 0 && page.modified == 1{
+                    notReferencedModified.append(ramPage)
+                } else if page.referenced == 1 && page.modified == 0{
+                    referencedNotModified.append(ramPage)
+                } else if page.referenced == 1 && page.modified == 1{
+                    referencedModified.append(ramPage)
+                }
+            }
+        }
+    }
+    
+    
+    if notReferencedNotModified.count > 0{
+        let objPage = Page()
+        objPage.pageId = notReferencedNotModified[0].pageId
+        objPage.pageNumber = HDD.count
+        
+        HDD.append(objPage)
+        
+        var pageIndex = 0
+        for i in 0...ram.count-1{
+            if ram[i].pageId == notReferencedNotModified[0].pageId && ram[i].pageNumber == notReferencedNotModified[0].pageNumber{
+                pageIndex = i
+                break
+            }
+        }
+        tempRAM.remove(at: pageIndex)
+            
+    }else if notReferencedModified.count > 0{
+        
+    }else if referencedNotModified.count > 0{
+        
+    }else if referencedModified.count > 0{
+        
+    }
+    
+    return tempRAM
+}
+
+
+func main(){
+    while control {
+        print("\n\n________________________________________________________________________")
+        print("MENU:")
+        print("1 - Жаңы барак кошуу")
+        print("2 - Баракка кайрылуу жасоо")
+        print("3 - Барактагы маалыматты өзгөртүү")
+        print("4 - Баракты жок кылуу")
+        print("5 - Баардык барактардын \"Referenced\" жана \"Modified\" биттеринин маанисин 0 кылуу")
+        print("6 - Эстин учурдагы абалын көрсөтүү")
+        print("0 - exit")
+        
+        
+        print("Please enter number: ")
+        let num = Int(readLine()!) ?? 7
+        
+        switch num{
+        case 1:
+            if RAM.count == sizeRAM && HDD.count == sizeHDD{
+                print("\n\nEs tolup kaldy.")
+            }else{
+                if RAM.count < sizeRAM{
+                    var emptyPageNumber = 0
+                    if RAM.count > 0{
+                        var ramPagesIndex = [Int]()
+                        for page in RAM{
+                            ramPagesIndex.append(page.pageNumber)
+                        }
+                        
+                        for i in 0...sizeRAM-1{
+                            if !ramPagesIndex.contains(i){
+                                emptyPageNumber = i
+                                break
+                            }
+                        }
+                    }
+                    pageID += 1
+                    let pageObj = Page()
+                    pageObj.pageId = pageID
+                    pageObj.pageNumber = emptyPageNumber
+                    RAM.append(pageObj)
+                    
+                    pageTables = completePageTable(RAM, HDD)
+                    print("\npage added succesfully!")
+                                        
+                }else{
+                    print("RAM toldu")
+                    RAM = notRecentlyUsedPage(RAM)
+                    
+                    var emptyPageNumber = 0
+                    if RAM.count > 0{
+                        var ramPagesIndex = [Int]()
+                        for page in RAM{
+                            ramPagesIndex.append(page.pageNumber)
+                        }
+                        
+                        for i in 0...sizeRAM-1{
+                            if !ramPagesIndex.contains(i){
+                                emptyPageNumber = i
+                                break
+                            }
+                        }
+                    }
+                    pageID += 1
+                    let pageObj = Page()
+                    pageObj.pageId = pageID
+                    pageObj.pageNumber = emptyPageNumber
+                    RAM.append(pageObj)
+                    pageTables = completePageTable(RAM, HDD)
+                    print("\npage added succesfully!")
+                }
+            }
+        case 2:
+            print("two")
+        case 3:
+            print("Enter the pageID: ")
+            let pageId = Int(readLine()!) ?? -1
+                        
+            var allPageId = [Int]()
+            for page in pageTables{
+                allPageId.append(page.pageId)
+            }
+            
+            if pageId > 0{
+                if allPageId.contains(pageId){
+                    var allPageIdRAM = [Int]()
+                    
+                    for page in RAM{
+                        allPageIdRAM.append(page.pageId)
+                    }
+                    if allPageIdRAM.contains(pageId){
+                        for page in pageTables{
+                            if page.pageId == pageId{
+                                page.modified = 1
+                                break
+                            }
+                        }
+                        print("\n\(pageId) - page modified succesfully!")
+                    }else{
+                        var objPage = Page()
+                        for page in HDD{
+                            if pageId == page.pageId{
+                                objPage = page
+                            }
+                        }
+                        
+                        var pageIndex = 0
+                        for i in 0...HDD.count-1{
+                            if HDD[i].pageId == pageId{
+                                pageIndex = i
+                                break
+                            }
+                        }
+                        HDD.remove(at: pageIndex)
+                        
+                        RAM = notRecentlyUsedPage(RAM)
+                        var emptyPageNumber = 0
+                        if RAM.count > 0{
+                            var ramPagesIndex = [Int]()
+                            for page in RAM{
+                                ramPagesIndex.append(page.pageNumber)
+                            }
+                            
+                            for i in 0...sizeRAM-1{
+                                if !ramPagesIndex.contains(i){
+                                    emptyPageNumber = i
+                                    break
+                                }
+                            }
+                        }
+                        
+                        objPage.pageNumber = emptyPageNumber
+                        RAM.append(objPage)
+                        pageTables = completePageTable(RAM, HDD)
+                        
+                        for page in pageTables{
+                            if page.pageId == pageId{
+                                page.modified = 1
+                                break
+                            }
+                        }
+                        print("\n\(pageId) - page modified succesfully!")
+                    }
+                }else{
+                    print("\nThere is not like this page!!!")
+                }
+            }else{
+                print("\nYour input is incorrect!!!")
+            }
+            
+        case 4:
+            print("Enter the pageID: ")
+            let pageId = Int(readLine()!) ?? -1
+                        
+            var allPageId = [Int]()
+            for page in pageTables{
+                allPageId.append(page.pageId)
+            }
+            
+            if pageId > 0{
+                if allPageId.contains(pageId){
+                    deletePage(pageId)
+                    pageTables = completePageTable(RAM, HDD)
+                    print("\n\(pageId) - page deleted succesfully!")
+                }else{
+                    print("\nThere is not like this page!!!")
+                }
+            }else{
+                print("\nYour input is incorrect!!!")
+            }
+            
+        case 5:
+            print("five:")
+            
+        case 6:
+            if RAM.count > 0{
+                print("\nRAM:\n")
+                print("Page ID\t\t| Page Number")
+                print("-------------------------")
+                for page in RAM{
+                    print("\t \(page.pageId)\t\t| \t\(page.pageNumber)")
+                }
+            } else{
+                print("\n\nRAM bosh")
+            }
+            
+            if HDD.count > 0{
+                print("\nHDD:\n")
+                print("Page ID\t\t| Page Number")
+                print("-------------------------")
+                for page in HDD{
+                    print("\t \(page.pageId)\t\t| \t\(page.pageNumber)")
+                }
+            } else{
+                print("\n\nHDD bosh")
+            }
+            
+            if pageTables.count > 0{
+                print("\n\nPage Tables:\n")
+                print("Page Location:  1 - RAM\n\t\t\t\t0 - HDD\n")
+
+                print("Page ID\t\t| Page Number\t| Page Location\t| Referenced\t| Modified")
+                print("-----------------------------------------------------------------------")
+                for page in pageTables{
+                    print("\t \(page.pageId)\t\t| \t\t\(page.pageNumber)\t\t| \t\t\(page.pageLocation)\t\t| \t\t\(page.referenced)\t\t| \t\(page.modified)")
+                }
+            }else{
+                print("Page Table bosh")
+            }
+            
+        case 0:
+            control = false
+        default:
+            print("\nYour input is incorrect!!!")
+        }
+    }
+}
+
+
+var RAM = [Page]()
+var HDD = [Page]()
+var pageTables = [PageTable]() // pageId, pageNumber, pageLocation, referenced, modified,
+let sizeRAM = 10
+let sizeHDD = 20
+var control = true
+var pageID = 0
+
+
+main()
